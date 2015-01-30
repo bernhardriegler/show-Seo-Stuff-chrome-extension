dbe3b35d9f448c602b47d62511799b327a9f3e5e
bernhardriegler <rieglerb@gmail.com>
2015-01-30 17:43:57 +0100

    source added
    manifest.json according to
    https://developer.chrome.com/extensions/manifest
    
    jquery for easy dom handling
    
    ext-showSEOstuff to grab and log stuff to console

---
+++ ext-showSEOstuff.js
@@ -0,0 +1,23 @@
+var ret = [];
+ret.push({name: "language", val: $('html').attr("lang")});
+ret.push({name: "title", val: $('head title').text()});
+ret.push({name: "description", val: $('head meta[name="description"]').attr('content')});
+ret.push({name: "viewport", val: $('head meta[name="viewport"]').attr('content')});
+// Opengraph i.e. Facebook
+ret.push({name: "og:title", val: $('head meta[property="og:title"]').attr('content')});
+ret.push({name: "og:description", val: $('head meta[property="og:description"]').attr('content')});
+ret.push({name: "og:image", val: $('head meta[property="og:image"]').attr('content')});
+ret.push({name: "og:site_name", val: $('head meta[property="og:site_name"]').attr('content')});
+ret.push({name: "og:type", val: $('head meta[property="og:type"]').attr('content')});
+// twitter
+ret.push({name: "twitter:title", val: $('head meta[name="twitter:title"]').attr('content')});
+ret.push({name: "twitter:description", val: $('head meta[name="twitter:description"]').attr('content')});
+ret.push({name: "twitter:card", val: $('head meta[name="twitter:card"]').attr('content')});
+ret.push({name: "twitter:site", val: $('head meta[name="twitter:site"]').attr('content')});
+ret.push({name: "twitter:creator", val: $('head meta[name="twitter:creator"]').attr('content')});
+// schema.org for google
+ret.push({name: "itemprop name", val: $('head meta[itemprop="name"]').attr('content')});
+ret.push({name: "itemprop description", val: $('head meta[itemprop="description"]').attr('content')});
+
+console.log("SEO?");
+console.table(ret);
